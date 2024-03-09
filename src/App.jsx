853dd3988ef1573aa32bc9df1d47dff3323595ce
Oussama-hamdi <86133676+Oussama-hamdi@users.noreply.github.com>
2024-03-09 15:17:21 +0100

    Added webpack.config file configurations

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// Minute 37:00
+import React from 'react';
+
+const App = () => {
+  return <div className="app">
+    <h1>Hello, React!</h1>
+  </div>
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    port: 3000,
+  },
+};
